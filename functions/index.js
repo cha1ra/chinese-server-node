c10a39ea41b6f3ff7adea5ebf8aefc61d6cc04d0
cha1ra <cha1ra.garnet@gmail.com>
2020-06-11 23:58:42 +0900

    feat: jsonの整理

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions')
 const express = require('express')
 
 // 中国語
+const OpenCC = require('opencc')
 const nodejieba = require('nodejieba')
 
 const app = express()
@@ -10,9 +11,35 @@ app.get('/hello', (req, res) => {
   res.send('Hello world!')
 })
 
-app.get('/chinese', (req, res) => {
+app.get('/chinese', async (req, res) => {
+  // クエリ関係
   const text = req.query.text
-  res.send(nodejieba.tag(text))
+  const isSimplified = req.query.isSimplified
+
+  // 翻訳関係
+  let simplified, traditional
+  console.log(isSimplified)
+  if (isSimplified === 1) {
+    const chineseConverter = new OpenCC('s2t.json')
+    simplified = text
+    traditional = await chineseConverter.convertPromise(text)
+  } else {
+    const chineseConverter = new OpenCC('t2s.json')
+    simplified = await chineseConverter.convertPromise(text)
+    traditional = text
+  }
+  const japaneseConverter = new OpenCC('t2jp.json')
+  const shinji = await japaneseConverter.convertPromise(traditional)
+
+  // レスポンス用オブジェクト作成
+  const responseJson = {
+    simplified,
+    traditional,
+    shinji,
+    words: nodejieba.tag(text)
+  }
+
+  res.send(responseJson)
 })
 
 const api = functions.https.onRequest(app)
