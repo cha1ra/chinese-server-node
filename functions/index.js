ae3e55f16fc6a53d6e95add2a77514b17318d6c2
cha1ra <cha1ra.garnet@gmail.com>
2020-06-11 22:50:50 +0900

    feat: setup nodejieba

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,18 @@
 const functions = require('firebase-functions')
 const express = require('express')
 
+// 中国語
+const nodejieba = require('nodejieba')
+
 const app = express()
 
 app.get('/hello', (req, res) => {
   res.send('Hello world!')
+})
+
+app.get('/chinese', (req, res) => {
+  const text = req.query.text
+  res.send(nodejieba.tag(text))
 })
 
 const api = functions.https.onRequest(app)
